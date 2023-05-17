2b288cb075ce08fae149411396cc4675162b0ed3
Prateek Verma <prateek4d@gmail.com>
2023-05-17 19:09:16 +0200

    figure5 codes

--- Figure5/model_FarmerGT_with_resistance1.m
+++ Figure5/model_FarmerGT_with_resistance1.m
@@ -1,4 +1,4 @@
-function dYdt = model_FarmerGT_with_resistance(t,Y,beta_w,beta_r,mu,eps_w,eps_r,N,theta)
+function dYdt = model_FarmerGT_with_resistance1(t,Y,beta_w,beta_r,mu,eps_w,eps_r,N,theta)
     Iuw = Y(1);
     Itw = Y(2);
     Iur = Y(3);
@@ -10,4 +10,5 @@ function dYdt = model_FarmerGT_with_resistance(t,Y,beta_w,beta_r,mu,eps_w,eps_r,
     dIur_dt = beta_r * (Iur+Itr) * Hu - mu*Iur;
     dItr_dt = beta_r * (1-eps_r) * (Iur+Itr) * Ht - mu*Itr;
     dYdt = [dIuw_dt,dItw_dt,dIur_dt,dItr_dt];
+    dYdt = dYdt';
 end
